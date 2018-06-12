17fbc58c3584b697964a1d20308cff85938a5761
Evan Kerr <evankrkerr90@gmail.com>
2018-06-12 03:42:37 +0000

    finished, added the ability to make avatars directory

--- download_avatars.js
+++ download_avatars.js
@@ -6,6 +6,7 @@ var repo = process.argv.slice(3);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//sets the url to be queried and the user information needed to access github api.
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -14,38 +15,40 @@ function getRepoContributors(repoOwner, repoName, cb) {
     'Authorization': authorization.GITHUB_TOKEN
     }
   }
-
-  if (!owner.length || !repo.length) {
-    console.log('Please specify a repo owner and a repo name.');
+  //exits the programs if user does not input a repo owner or repo name.
+  if (!owner.length && !repo.length) {
+    console.log('Please specify a repo owner followed by a repo name.');
     return;
-  }
+  };
 
+  //retrieves collaborator information from url and turns it into an array of objects.
   request(options, function(err, res, body) {
     objAvatars = JSON.parse(body);
-    return cb(err, objAvatars)
+    cb(err, objAvatars);
   });
 }
 
-
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-  .on('end', function() {
-    console.log('Pictures saved to ' + filePath);
+//takes the image urls and filepath from the callback function and writes the images to the filepath.
+  function downloadImageByURL(url, filePath) {
+    if (!fs.existsSync('avatars')) {
+      fs.mkdirSync('avatars');
+    }
+    request.get(url)
+      .on('response', function() {
+      console.log('Saving picture to ' + filePath);
   })
   .pipe(fs.createWriteStream(filePath));
-}
+  }
 
+//takes the array of objects objAvatars and loops through them to retrieve image urls as well as the naming for the filepath.
 getRepoContributors(owner, repo, function(err, result) {
-  //console.log("Errors:", err);
-  //console.log("Result:", result);
     result.forEach(function(element) {
     var imageURL = element.avatar_url;
-    var filePathName = './avatars/' + element.login + '.jpg';
+    var filePathName = 'avatars/' + element.login + '.jpg';
       downloadImageByURL(imageURL, filePathName);
-  });
+    });
 });
 
-//downloadImageByURL("https://avatars3.githubusercontent.com/u/46987?v=4", './avatars/' )
 
 
 
